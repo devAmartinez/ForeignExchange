b51346cd183a8ea73f1a78829bdf4f0b2cf4a2b7
Jhonatan Acevedo Martínez <mravast.martinez@gmail.com>
2017-11-13 07:06:04 -0500

    Se añade primera parte de la impelentación de base de datos SQLite

--- ForeignExchange/ForeignExchange/Models/Rate.cs
+++ ForeignExchange/ForeignExchange/Models/Rate.cs
@@ -1,7 +1,11 @@
 ﻿namespace ForeignExchange.Models
 {
+    using SQLite.Net.Attributes;
+
     public class Rate
     {
+        [PrimaryKey]
+
         public int RateId { get; set; }
 
         public string Code { get; set; }
@@ -9,5 +13,10 @@
         public double TaxRate { get; set; }
 
         public string Name { get; set; }
+
+        public override int GetHashCode()
+        {
+            return RateId;
+        }
     }
 }
